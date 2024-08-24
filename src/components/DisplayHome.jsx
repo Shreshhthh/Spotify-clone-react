9ad2f30ac8fcd5f76141fddb3290ae54c3f1178f
Shreshth Awasthi <shreshthawathiab@gmail.com>
2024-08-25 02:57:41 +0530

    first commit

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import { songsData } from "../assets/assets";
+import SongsItems from "./SongsItems";
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongsItems
+              key={index}
+              image={item.image}
+              desc={item.desc}
+              name={item.name}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
